c0416869967a84e0b666f7985095e79ba2b54d03
GANGISETTY RAJ CHARAN <97686664+rajcharanthecoder@users.noreply.github.com>
2022-10-11 12:50:06 +0530

    Add files via upload

--- Go Language/main.go
+++ Go Language/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt" //fmt is a package that implements formatted I/O with functions analogous to C's printf and scanf.
+import "fmt"
+
+var score = 99.5
+
+// cannot use shorthand outside of functions
+// scoreTwo := 50
 
 func main() {
-	var age int = 20
-	fmt.Println("Hello, you are", age, "years old.")
-	fmt.Println("Hello, World!")
+	sayHello("mario")
+	showScore()
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
 }
